016d0278e25016b4653a8b2bd40cb6ed08dfde50
MHDFahz <fahis.skazi@gmail.com>
2021-04-18 23:37:34 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const morgan = require('morgan');
 
-require('dotenv').config();
+require('dotenv').config({
+    path: "./config/config.env"
+})
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors());        //running api in reactjs from server without any error while json transferring
+app.use(morgan('dev'))  //Give Information about each request
+
+connectDB()             //Mongo DB Connection
+
+//Load all route
+const authRouter = require('./routes/auth.route');
+
+//Use Route
+app.use('/api/',authRouter)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
---
+++ routes/auth.route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// Load Controller
+const {
+    registerController
+} = require('../controllers/auth.controller.js')
+
+router.post('/register',registerController)
+
+module.exports = router
